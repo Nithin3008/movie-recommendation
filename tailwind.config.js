167d958dc2cd29f74f264ddf558e9387cc0289bf
Nithin <nithindevathi@gmail.com>
2023-07-14 10:47:30 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primClr: "#0f171e",
+        secClr: "#1a242f",
+        fontPrim: "#fffff",
+        fontSec: "#79b8f3",
+      },
+    },
+  },
+  plugins: [],
+};
